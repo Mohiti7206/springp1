6f9056add66d07c81d43bf966117263c1c9ad373
mohit kumar <mohiti7206@gmial.com>
2023-10-29 08:58:38 +0530

    15th video

--- com.springcore/src/main/java/com/springcore/lifecycle/Test.java
+++ com.springcore/src/main/java/com/springcore/lifecycle/Test.java
@@ -1,5 +1,4 @@
 package com.springcore.lifecycle;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
@@ -13,10 +12,24 @@ public class Test {
 		// TODO Auto-generated method stub
 		
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/lifecycle/config.xml");
-		Samosa s1 = (Samosa) context.getBean("s1");
-		System.out.println(s1);
+		
+		/*
+		 * Samosa s1 = (Samosa) context.getBean("s1"); System.out.println(s1);
+		 */
 		context.registerShutdownHook();
 		
+		System.out.println("------------------");
+		
+		
+		Pepsi obj = (Pepsi)context.getBean("p1");
+		
+		System.out.println(obj);
+		
+		
+		
+		
+		
+	
 		
 		
 		
